fb5678141f5bc7bb912e07d750818c9aeb3f1e3f
Viktor Hellström <anfly0@gmail.com>
2020-08-04 22:17:25 +0200

    Adds alias to ExBankID.Cancel

--- lib/ex_bank_id/cancel.ex
+++ lib/ex_bank_id/cancel.ex
@@ -1,23 +1,24 @@
 defmodule ExBankID.Cancel do
+  alias ExBankID.Cancel.Payload
   def cancel(token, opts \\ [])
 
   def cancel(token, opts) when is_binary(token) do
-    with payload = %ExBankID.Cancel.Payload{} <-
-           ExBankID.Cancel.Payload.new(token) do
+    with payload = %Payload{} <-
+           Payload.new(token) do
       ExBankID.HttpRequest.send_request(payload, opts)
     end
   end
 
   def cancel(token = %ExBankID.Auth.Response{}, opts) do
-    with payload = %ExBankID.Cancel.Payload{} <-
-           ExBankID.Cancel.Payload.new(token) do
+    with payload = %Payload{} <-
+           Payload.new(token) do
       ExBankID.HttpRequest.send_request(payload, opts)
     end
   end
 
   def cancel(token = %ExBankID.Sign.Response{}, opts) do
-    with payload = %ExBankID.Cancel.Payload{} <-
-           ExBankID.Cancel.Payload.new(token) do
+    with payload = %Payload{} <-
+           Payload.new(token) do
       ExBankID.HttpRequest.send_request(payload, opts)
     end
   end
