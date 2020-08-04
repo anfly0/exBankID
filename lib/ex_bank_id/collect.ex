e6fb5a3354667069dc82afa3a8ac7db9d2fdf5e4
Viktor Hellström <anfly0@gmail.com>
2020-08-04 22:17:41 +0200

    Adds alias to ExBankID.Collect

--- lib/ex_bank_id/collect.ex
+++ lib/ex_bank_id/collect.ex
@@ -1,23 +1,24 @@
 defmodule ExBankID.Collect do
+  alias ExBankID.Collect.Payload
   def collect(token, opts \\ [])
 
   def collect(token, opts) when is_binary(token) do
-    with payload = %ExBankID.Collect.Payload{} <-
-           ExBankID.Collect.Payload.new(token) do
+    with payload = %Payload{} <-
+           Payload.new(token) do
       ExBankID.HttpRequest.send_request(payload, opts)
     end
   end
 
   def collect(token = %ExBankID.Auth.Response{}, opts) do
-    with payload = %ExBankID.Collect.Payload{} <-
-           ExBankID.Collect.Payload.new(token) do
+    with payload = %Payload{} <-
+           Payload.new(token) do
       ExBankID.HttpRequest.send_request(payload, opts)
     end
   end
 
   def collect(token = %ExBankID.Sign.Response{}, opts) do
-    with payload = %ExBankID.Collect.Payload{} <-
-           ExBankID.Collect.Payload.new(token) do
+    with payload = %Payload{} <-
+           Payload.new(token) do
       ExBankID.HttpRequest.send_request(payload, opts)
     end
   end
