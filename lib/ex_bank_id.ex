324d11cc31d839e1071607499462c202926e6030
Viktor Hellström <anfly0@gmail.com>
2020-08-05 20:15:05 +0200

    Adds specs to ExBankID

--- lib/ex_bank_id.ex
+++ lib/ex_bank_id.ex
@@ -3,11 +3,28 @@ defmodule ExBankID do
   Simple abstraction over the swedish BankID API.
   """
 
+  @spec auth(String.t(), url: String.t(), cert_file: String.t()) ::
+          {:error, %ExBankID.Error.Api{} | binary()} | {:ok, %ExBankID.Auth.Response{}}
   defdelegate auth(ip_address, opts \\ []), to: ExBankID.Auth
 
-  defdelegate collect(order_ref, opts \\ []), to: ExBankID.Collect
-
+  @spec sign(String.t(), String.t(),
+          url: String.t(),
+          cert_file: String.t(),
+          personal_number: String.t(),
+          user_non_visible_data: String.t()
+        ) :: {:error, %ExBankID.Error.Api{} | binary()} | {:ok, %ExBankID.Sign.Response{}}
   defdelegate sign(ip_address, user_visible_data, opts \\ []), to: ExBankID.Sign
 
+  @spec collect(String.t() | %ExBankID.Sign.Response{} | %ExBankID.Auth.Response{},
+          url: String.t(),
+          cert_file: String.t()
+        ) ::
+          {:error, %ExBankID.Error.Api{} | binary()} | {:ok, %ExBankID.Collect.Response{}}
+  defdelegate collect(order_ref, opts \\ []), to: ExBankID.Collect
+
+  @spec cancel(String.t() | %ExBankID.Sign.Response{} | %ExBankID.Auth.Response{},
+          url: String.t(),
+          cert_file: String.t()
+        ) :: {:error, %ExBankID.Error.Api{} | binary()} | {:ok, %{}}
   defdelegate cancel(order_ref, opts \\ []), to: ExBankID.Cancel
 end
