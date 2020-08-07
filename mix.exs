a4b6fdf42cc7aa51da1b8241c176ca421387f9ed
Viktor Hellström <anfly0@gmail.com>
2020-08-07 11:57:39 +0200

    Adds dialyxir

--- mix.exs
+++ mix.exs
@@ -25,6 +25,7 @@ defmodule ExBankID.MixProject do
       {:poison, "~> 3.1"},
       {:uuid, "~> 1.1"},
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:bypass, "~> 1.0", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
