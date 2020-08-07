eb2b7e7d71e25f3e29ada3bbd699b7920c9d3784
Viktor Hellström <anfly0@gmail.com>
2020-08-07 11:58:21 +0200

    Adds doc tests to Cancel Payload

--- lib/ex_bank_id/cancel/payload.ex
+++ lib/ex_bank_id/cancel/payload.ex
@@ -4,7 +4,23 @@ defmodule ExBankID.Cancel.Payload do
   """
   defstruct [:orderRef]
 
-  @spec new(binary | ExBankID.Auth.Response.t()) ::
+  @doc """
+  Returns a Cancel Payload given a orderRef, Auth response struct or Sign response struct
+
+  ## Examples
+    iex> ExBankID.Cancel.Payload.new("131daac9-16c6-4618-beb0-365768f37288")
+    %ExBankID.Cancel.Payload{orderRef: "131daac9-16c6-4618-beb0-365768f37288"}
+
+    iex> ExBankID.Cancel.Payload.new("Not-a-valid-UUID")
+    {:error, "OrderRef is not a valid UUID"}
+
+    iex> %ExBankID.Auth.Response{orderRef: "131daac9-16c6-4618-beb0-365768f37288"} |> ExBankID.Cancel.Payload.new()
+    %ExBankID.Cancel.Payload{orderRef: "131daac9-16c6-4618-beb0-365768f37288"}
+
+    iex> %ExBankID.Sign.Response{orderRef: "131daac9-16c6-4618-beb0-365768f37288"} |> ExBankID.Cancel.Payload.new()
+    %ExBankID.Cancel.Payload{orderRef: "131daac9-16c6-4618-beb0-365768f37288"}
+  """
+  @spec new(binary | %ExBankID.Auth.Response{} | %ExBankID.Sign.Response{}) ::
           {:error, String.t()} | %__MODULE__{orderRef: String.t()}
   def new(%ExBankID.Auth.Response{orderRef: order_ref}) when is_binary(order_ref) do
     %ExBankID.Cancel.Payload{}
--- test/doc/doc_test.exs
+++ test/doc/doc_test.exs
@@ -2,4 +2,5 @@ defmodule ExBankID.Auth.PayloadTest do
   use ExUnit.Case, async: true
   doctest ExBankID.Auth.Payload
   doctest ExBankID.Sign.Payload
+  doctest ExBankID.Cancel.Payload
 end
