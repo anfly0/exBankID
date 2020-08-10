5fb5b3202deb6171f90163269fc4a2d8ff7c10cf
Viktor Hellström <anfly0@gmail.com>
2020-08-10 12:57:12 +0200

    !!! Adds options check to ExBankID.sign  -- cert_file is now mandatory

--- lib/ex_bank_id/sign.ex
+++ lib/ex_bank_id/sign.ex
@@ -1,9 +1,28 @@
 defmodule ExBankID.Sign do
   alias ExBankID.Sign
 
+  @options [
+    url: [
+      type: :string,
+      default: "https://appapi2.test.bankid.com/rp/v5.1/"
+    ],
+    cert_file: [
+      type: :string,
+      required: true
+    ],
+    personal_number: [
+      type: :string
+      # TODO: Add validator
+    ],
+    user_non_visible_data: [
+      type: :string
+    ]
+  ]
+
   def sign(ip_address, user_visible_data, opts \\ [])
       when is_binary(ip_address) and is_binary(user_visible_data) and is_list(opts) do
-    with payload = %Sign.Payload{} <- Sign.Payload.new(ip_address, user_visible_data, opts) do
+    with {:ok, opts} <- NimbleOptions.validate(opts, @options),
+         payload = %Sign.Payload{} <- Sign.Payload.new(ip_address, user_visible_data, opts) do
       ExBankID.HttpRequest.send_request(payload, opts)
     end
   end
