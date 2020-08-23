2b93b7dd44a68d058c68d662d322095a66a7df77
Viktor Hellström <anfly0@gmail.com>
2020-08-23 12:50:11 +0200

    Makes HTTPosion optional

--- lib/ex_bank_id/http/default/httpoison.ex
+++ lib/ex_bank_id/http/default/httpoison.ex
@@ -1,20 +1,22 @@
-defmodule ExBankID.Http.Default do
-  @moduledoc """
-  HTTPoison implementation of the ExBankID.Http.Client behaviour.
-  """
+if Code.ensure_loaded?(HTTPoison) do
+  defmodule ExBankID.Http.Default do
+    @moduledoc """
+    HTTPoison implementation of the ExBankID.Http.Client behaviour.
+    """
 
-  @behaviour ExBankID.Http.Client
-  alias ExBankID.Http.Response
+    @behaviour ExBankID.Http.Client
+    alias ExBankID.Http.Response
 
-  def post(url, req_body, headers, cert_file, http_opts \\ []) do
-    opts = Keyword.put(http_opts, :ssl, certfile: cert_file)
+    def post(url, req_body, headers, cert_file, http_opts \\ []) do
+      opts = Keyword.put(http_opts, :ssl, certfile: cert_file)
 
-    case HTTPoison.post(url, req_body, headers, opts) do
-      {:ok, %HTTPoison.Response{status_code: code, body: body}} ->
-        {:ok, %Response{status_code: code, body: body}}
+      case HTTPoison.post(url, req_body, headers, opts) do
+        {:ok, %HTTPoison.Response{status_code: code, body: body}} ->
+          {:ok, %Response{status_code: code, body: body}}
 
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
+        {:error, %HTTPoison.Error{reason: reason}} ->
+          {:error, reason}
+      end
     end
   end
 end
