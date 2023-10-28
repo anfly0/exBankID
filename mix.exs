66b7379a7ce9321744a72d613b73bc5d0128658d
Carl Gleisner <carlgleisner@users.noreply.github.com>
2023-10-28 08:10:33 +0200

    Upgrade credo.

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule ExBankID.MixProject do
       {:httpoison, "~> 1.8", optional: true},
       {:poison, "~> 4.0 or ~> 3.1", optional: true},
       {:uuid, "~> 1.1"},
-      {:credo, "~> 1.4", only: [:dev], runtime: false},
+      {:credo, "~> 1.6", only: [:dev], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:bypass, "~> 2.0", only: :test},
       {:excoveralls, "~> 0.10", only: :test},
